e9c772a950ee4dbafcfd8e43e683c8dc0fa13417
Adrian Ulbrich <adrian.ulbrich.2003@gmail.com>
2024-01-12 17:50:53 +0100

    Complete App class declaration

--- Include/App.hpp
+++ Include/App.hpp
@@ -1,8 +1,41 @@
 #pragma once
 
 #include <UplinkObject.hpp>
+#include <TempDefines.hpp>
 
 class App : UplinkObject
 {
-	
+public:
+	static constexpr size_t PathMax = 0x100;
+	static constexpr size_t VersionMax = 0x20;
+	static constexpr size_t TypeMax = 0x20;
+	static constexpr size_t DateMax = 0x20;
+	static constexpr size_t TitleMax = 0x40;
+	static constexpr size_t BuildMax = 0x100;
+
+private:
+	char Path[PathMax];
+	char UsersPath[PathMax];
+	char UsersTempPath[PathMax];
+	char UsersOldPath[PathMax];
+	char Version[VersionMax];
+	char Type[TypeMax];
+	char Date[DateMax];
+	char Title[TitleMax];
+	char Build[BuildMax];
+	int InitTime;
+	bool Closed;
+	Options* TheOptions;
+	Network* TheNetwork;
+	MainMenu* TheMainMenu;
+	PhoneDialler* ThePhoneDialler;
+	char* NextLoadGame;
+	bool RequireCodeCard;
+
+	virtual ~App() override;
+	virtual bool Load() override;
+	virtual void Save() override;
+	virtual void Print() override;
+	virtual void Update() override;
+	virtual char* GetID() override;
 };
--- Include/TempDefines.hpp
+++ Include/TempDefines.hpp
@@ -1,5 +1,10 @@
 #pragma once
 
+typedef void Options;
+typedef void Network;
+typedef void MainMenu;
+typedef void PhoneDialler;
+
 #if defined(__x86_64__)
 #include <TempDefines_x64.hpp>
 #elif defined(__i386__)
