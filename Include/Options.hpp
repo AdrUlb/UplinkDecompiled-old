ce7a5fa87f97fad4462153db7c187cf6515fa4ac
Adrian Ulbrich <adrian.ulbrich.2003@gmail.com>
2024-01-14 09:46:41 +0100

    Create Options declaration

--- Include/Options.hpp
+++ Include/Options.hpp
@@ -1,3 +1,12 @@
 #pragma once
 
-typedef void Options;
+#include <UplinkObject.hpp>
+
+class Options : UplinkObject
+{
+	virtual ~Options() override;
+	virtual bool Load() override;
+	virtual void Save() override;
+	virtual void Print() override;
+	virtual const char* GetID() override;
+};
